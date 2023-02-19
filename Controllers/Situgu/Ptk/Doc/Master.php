9f6815e706c0678a38f303d7237cd7226b6888f4
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-02-19 21:48:22 +0700

    fix doc ptk

--- Controllers/Situgu/Ptk/Doc/Master.php
+++ Controllers/Situgu/Ptk/Doc/Master.php
@@ -43,7 +43,12 @@ class Master extends BaseController
 
         $data['user'] = $user->data;
         $id = $this->_helpLib->getPtkId($user->data->id);
-        $data['ptk'] = $this->_db->table('_ptk_tb')->where('id', $id)->get()->getRowObject();
+        $ptk = $this->_db->table('_ptk_tb')->where('id', $id)->get()->getRowObject();
+        if (!$ptk) {
+            return view('404', $data);
+        }
+
+        $data['ptk'] = $ptk;
 
         return view('situgu/ptk/doc/master/index', $data);
     }
