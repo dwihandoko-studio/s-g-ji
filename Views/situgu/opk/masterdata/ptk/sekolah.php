6748de9178368d16cc15de0cb710de7fb6b2a663
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-02-22 12:53:33 +0700

    fix data ptk opk

--- Views/situgu/opk/masterdata/ptk/sekolah.php
+++ Views/situgu/opk/masterdata/ptk/sekolah.php
@@ -10,11 +10,12 @@
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                     <h4 class="mb-sm-0 font-size-18">PTK</h4>
 
-                    <!-- <div class="page-title-right">
+                    <div class="page-title-right">
                         <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript:actionSyncAll(this);" class="btn btn-primary btn-rounded waves-effect waves-light">Tarik Semua Data PTK Dari Dapodik</a></li>
+                            <li class="breadcrumb-item"><a href="./data" class="btn btn-primary btn-rounded waves-effect waves-light">Kembali</a></li>
+                            <!-- <li class="breadcrumb-item"><a href="javascript:actionSyncAll(this);" class="btn btn-primary btn-rounded waves-effect waves-light">Tarik Semua Data PTK Dari Dapodik</a></li> -->
                         </ol>
-                    </div> -->
+                    </div>
 
                 </div>
             </div>
