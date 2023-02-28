9484eb7e9f25678b381858ec34b2a6b803b4d763
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-02-28 20:58:07 +0700

    disabled wa verification

--- Models/Situgu/Opsr/VerifikasiModel.php
+++ Models/Situgu/Opsr/VerifikasiModel.php
@@ -54,7 +54,7 @@ class VerifikasiModel extends Model
         $this->dt->where('a.jenis_tunjangan', $jenis);
         $this->dt->where('a.status_usulan', 0);
         $this->dt->where('a.id_tahun_tw', $this->request->getPost('tw'));
-        $this->dt->whereIn('bnpsn', $npsns);
+        $this->dt->whereIn('b.npsn', $npsns);
         // $this->dt->where('b.npsn', $npsn);
         $this->_get_datatables_query();
         if ($this->request->getPost('length') != -1)
