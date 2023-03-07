0af3f0e9632ff021f292a834efbe1677dee5616a
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-03-07 21:50:09 +0700

    fix sptjm

--- Libraries/Downloadlib.php
+++ Libraries/Downloadlib.php
@@ -35,43 +35,28 @@ class Downloadlib
             // exec('libreoffice --headless --convert-to pdf ' . $path . ' --outdir ' . $dir, $output, $retval);
 
             $command = 'libreoffice --headless --convert-to pdf ' . $path . ' --outdir ' . $dir;
-            exec('echo "bejo123" | sudo -S -u bejo && "' . $command . '"', $output, $retval);
+            // exec('echo "bejo123" | sudo -S -u bejo && "' . $command . '"', $output, $retval);
+            $output = shell_exec('sudo -u bejo -p bejo123 ' . $command);
+            echo "<pre>$output</pre>";
+            die;
             // ) {
-            if ($retval === 0) {
-                $file = $dir . '/' . $name;
-                $response = new \stdClass;
-                $response->status = 200;
-                $response->message = "Berhasil convert file.";
-                $response->file = $file;
-                return $response;
-            } else {
-                $response = new \stdClass;
-                $response->status = 400;
-                $response->var_dump = var_dump($output);
-                $response->retval = var_dump($retval);
-                $response->error = "Error converting file to PDF: " . implode("\n", $output);
-                $response->message = "Gagal convert file.";
-                return $response;
-            }
-            // if (exec('libreoffice --headless --convert-to pdf ' . $dirUploadWord . '/' . $newNamelampiran . ' --outdir ' . $dir, $output, $retval)) {
-            // $file = $dir . '/' . $name;
-            // $this->response->setHeader('Content-Type', 'application/octet-stream');
-            // $this->response->setHeader('Content-Disposition', 'attachment; filename="' . $name . '"');
-            // $this->response->setHeader('Content-Length', filesize($file));
-            // return $file;
-            // $response = new \stdClass;
-            // $response->status = 200;
-            // $response->message = "Berhasil convert file.";
-            // $response->file = $file;
-            // return $response;
-
-            // return $this->response->sendFile($file);
+            // if ($retval === 0) {
+            //     $file = $dir . '/' . $name;
+            //     $response = new \stdClass;
+            //     $response->status = 200;
+            //     $response->message = "Berhasil convert file.";
+            //     $response->file = $file;
+            //     return $response;
             // } else {
             //     $response = new \stdClass;
             //     $response->status = 400;
+            //     $response->var_dump = var_dump($output);
+            //     $response->retval = var_dump($retval);
+            //     $response->error = "Error converting file to PDF: " . implode("\n", $output);
             //     $response->message = "Gagal convert file.";
             //     return $response;
             // }
+
         } catch (\Exception $err) {
             $response = new \stdClass;
             $response->status = 400;
