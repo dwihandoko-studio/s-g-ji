e3af80d2f253e38fbdacfd8ccb61b470284ad6b1
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-03-10 16:02:55 +0700

    fix page usulan adm

--- Controllers/Situgu/Opk/Verifikasi/Tpg.php
+++ Controllers/Situgu/Opk/Verifikasi/Tpg.php
@@ -64,6 +64,8 @@ class Tpg extends BaseController
         }
 
         $kecamatan = $this->_helpLib->getKecamatan($userId);
+        var_dump($kecamatan);
+        die;
         $npsns = $this->_helpLib->getSekolahKecamatanString($kecamatan, [5]);
 
         $lists = $datamodel->get_datatables($npsns, 'tpg');
