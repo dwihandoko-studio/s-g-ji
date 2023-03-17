7f7d2b75bdb928d4604789b92d9b445a01e7f23d
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-03-17 09:04:28 +0700

    fix nominal sptjm

--- Controllers/Situgu/Su/Profil.php
+++ Controllers/Situgu/Su/Profil.php
@@ -31,7 +31,7 @@ class Profil extends BaseController
         $us_gaji_pokok = 1000000;
         $pph21 = (15 / 100);
 
-        var_dump(rpTanpaAwalan((($us_gaji_pokok * 3) * $pph21)) - ($us_gaji_pokok * 3));
+        var_dump(rpTanpaAwalan(($us_gaji_pokok * 3) - (($us_gaji_pokok * 3) * $pph21)));
         die;
 
         return redirect()->to(base_url('situgu/su/profil/data'));
