d68eda33c5f2a4f3d244ef24df28f396f75ca6ce
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-03-27 00:42:58 +0700

    fix add informasi

--- Controllers/Situgu/Su/Setting/Informasi.php
+++ Controllers/Situgu/Su/Setting/Informasi.php
@@ -37,9 +37,9 @@ class Informasi extends BaseController
             $action = '<a href="javascript:actionDetail(\'' . $list->id . '\', \'' . str_replace("'", "", $list->judul) . '\');"><button type="button" class="btn btn-primary btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
                         <i class="bx bxs-show font-size-16 align-middle"></i></button>
                         </a>
-                        <a href="javascript:actionEdit(\'' . $list->id . '\', \'' . str_replace("'", "", $list->judul) . '\');"><button type="button" class="btn btn-secondary btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
+                        <!--<a href="javascript:actionEdit(\'' . $list->id . '\', \'' . str_replace("'", "", $list->judul) . '\');"><button type="button" class="btn btn-secondary btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
                         <i class="bx bx-edit font-size-16 align-middle"></i></button>
-                        </a>
+                        </a>-->
                         <a href="javascript:actionHapus(\'' . $list->id . '\', \'' . str_replace("'", "", $list->judul) . '\');" class="delete" id="delete"><button type="button" class="btn btn-danger btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
                         <i class="bx bx-trash font-size-16 align-middle"></i></button>
                         </a>';
