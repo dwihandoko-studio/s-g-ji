88ba3d56bafc22e96fbad5fd6d01921fa05641bd
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-04-01 20:53:36 +0700

    fix sptjm opk

--- Models/Situgu/Su/Tpg/DitolakModel.php
+++ Models/Situgu/Su/Tpg/DitolakModel.php
@@ -49,7 +49,7 @@ class DitolakModel extends Model
     }
     function get_datatables()
     {
-        $this->dt->select("a.id as id_usulan, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan");
+        $this->dt->select("a.id as id_usulan, a.date_reject, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan");
         $this->dt->join('_ptk_tb b', 'a.id_ptk = b.id');
         $this->dt->whereIn('a.status_usulan', [3, 4]);
         if ($this->request->getPost('tw')) {
@@ -67,7 +67,7 @@ class DitolakModel extends Model
 
     function count_filtered()
     {
-        $this->dt->select("a.id as id_usulan, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan");
+        $this->dt->select("a.id as id_usulan, a.date_reject, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan");
         $this->dt->join('_ptk_tb b', 'a.id_ptk = b.id');
         $this->dt->whereIn('a.status_usulan', [3, 4]);
         if ($this->request->getPost('tw')) {
@@ -83,7 +83,7 @@ class DitolakModel extends Model
 
     public function count_all()
     {
-        $this->dt->select("a.id as id_usulan, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan");
+        $this->dt->select("a.id as id_usulan, a.date_reject, a.kode_usulan, a.id_ptk, a.id_tahun_tw, a.status_usulan, a.date_approve_sptjm, b.nama, b.nik, b.nuptk, b.jenis_ptk, b.kecamatan");
         $this->dt->join('_ptk_tb b', 'a.id_ptk = b.id');
         $this->dt->whereIn('a.status_usulan', [3, 4]);
         if ($this->request->getPost('tw')) {
