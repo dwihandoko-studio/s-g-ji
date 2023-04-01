70743fb33d5eafa70388fe73612d7010b06225b9
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-04-01 22:15:41 +0700

    fix sptjm opsr

---
+++ Controllers/Situgu/Su/Us/Tpg/Lolosberkaskembali.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace App\Controllers\Situgu\Su\Us\Tpg;
+
+use App\Controllers\BaseController;
+use App\Models\Situgu\Su\Tpg\LolosberkasModel;
+use Config\Services;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use App\Libraries\Profilelib;
+use App\Libraries\Apilib;
+use App\Libraries\Helplib;
+use App\Libraries\Situgu\Kehadiranptklib;
+use App\Libraries\Uuid;
+
+class Lolosberkaskembali extends BaseController
+{
+    var $folderImage = 'masterdata';
+    private $_db;
+    private $model;
+    private $_helpLib;
+
+    function __construct()
+    {
+        helper(['text', 'file', 'form', 'session', 'array', 'imageurl', 'web', 'filesystem']);
+        $this->_db      = \Config\Database::connect();
+        $this->_helpLib = new Helplib();
+    }
+
+    public function getAll()
+    {
+        $request = Services::request();
+        $datamodel = new LolosberkasModel($request);
+
+        $jwt = get_cookie('jwt');
+        $token_jwt = getenv('token_jwt.default.key');
+        if ($jwt) {
+            try {
+                $decoded = JWT::decode($jwt, new Key($token_jwt, 'HS256'));
+                if ($decoded) {
+                    $userId = $decoded->id;
+                    $level = $decoded->level;
+                } else {
+                    $output = [
+                        "draw" => $request->getPost('draw'),
+                        "recordsTotal" => 0,
+                        "recordsFiltered" => 0,
+                        "data" => []
+                    ];
+                    echo json_encode($output);
+                    return;
+                }
+            } catch (\Exception $e) {
+                $output = [
+                    "draw" => $request->getPost('draw'),
+                    "recordsTotal" => 0,
+                    "recordsFiltered" => 0,
+                    "data" => []
+                ];
+                echo json_encode($output);
+                return;
+            }
+        }
+
+        $lists = $datamodel->get_datatables();
+        $data = [];
+        $no = $request->getPost("start");
+        foreach ($lists as $list) {
+            $no++;
+            $row = [];
+
+            $row[] = $no;
+            // $action = '<div class="btn-group">
+            //             <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Action <i class="mdi mdi-chevron-down"></i></button>
+            //             <div class="dropdown-menu" style="">
+            //                 <a class="dropdown-item" href="javascript:actionDetail(\'' . $list->id . '\', \'' . str_replace("'", "", $list->nama) . '\');"><i class="bx bxs-show font-size-16 align-middle"></i> &nbsp;Detail</a>
+            //                 <a class="dropdown-item" href="javascript:actionSync(\'' . $list->id . '\', \'' . $list->id_ptk . '\', \'' . str_replace("'", "", $list->nama)  . '\', \'' . $list->nuptk  . '\', \'' . $list->npsn . '\');"><i class="bx bx-transfer-alt font-size-16 align-middle"></i> &nbsp;Sync Dapodik</a>
+            //             </div>
+            //         </div>';
+            $action = '<a href="javascript:actionKembali(\'' . $list->id_usulan . '\', \'' . $list->id_ptk . '\', \'' . $list->id_tahun_tw . '\', \'' . str_replace('&#039;', "`", str_replace("'", "`", $list->nama)) . '\');"><button type="button" class="btn btn-primary btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
+                <i class="bx bx-undo font-size-16 align-middle"></i> Kembalikan</button>
+                </a>';
+            //     <a href="javascript:actionSync(\'' . $list->id . '\', \'' . $list->id_ptk . '\', \'' . str_replace("'", "", $list->nama)  . '\', \'' . $list->nuptk  . '\', \'' . $list->npsn . '\');"><button type="button" class="btn btn-secondary btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
+            //     <i class="bx bx-transfer-alt font-size-16 align-middle"></i></button>
+            //     </a>
+            //     <a href="javascript:actionHapus(\'' . $list->id . '\', \'' . str_replace("'", "", $list->nama)  . '\', \'' . $list->nuptk . '\');" class="delete" id="delete"><button type="button" class="btn btn-danger btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
+            //     <i class="bx bx-trash font-size-16 align-middle"></i></button>
+            //     </a>';
+            $row[] = $action;
+            $row[] = str_replace('&#039;', "`", str_replace("'", "`", $list->nama));
+            $row[] = $list->nik;
+            $row[] = $list->nuptk;
+            $row[] = $list->jenis_ptk;
+            $row[] = $list->date_approve;
+
+            $data[] = $row;
+        }
+        $output = [
+            "draw" => $request->getPost('draw'),
+            "recordsTotal" => $datamodel->count_all(),
+            "recordsFiltered" => $datamodel->count_filtered(),
+            "data" => $data
+        ];
+        echo json_encode($output);
+    }
+
+    public function index()
+    {
+        return redirect()->to(base_url('situgu/su/us/tpg/lolosberkaskembali/data'));
+    }
+
+    public function data()
+    {
+        $data['title'] = 'USULAN LOLOS BERKAS TPG';
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->status != 200) {
+            delete_cookie('jwt');
+            session()->destroy();
+            return redirect()->to(base_url('auth'));
+        }
+        $id = $this->_helpLib->getPtkId($user->data->id);
+        $data['user'] = $user->data;
+        $data['tw'] = $this->_db->table('_ref_tahun_tw')->where('is_current', 1)->orderBy('tahun', 'desc')->orderBy('tw', 'desc')->get()->getRowObject();
+        $data['tws'] = $this->_db->table('_ref_tahun_tw')->orderBy('tahun', 'desc')->orderBy('tw', 'desc')->get()->getRowObject();
+        return view('situgu/su/us/tpg/lolosberkaskembali/index', $data);
+    }
+
+    public function kembalikan()
+    {
+        if ($this->request->getMethod() != 'post') {
+            $response = new \stdClass;
+            $response->status = 400;
+            $response->message = "Permintaan tidak diizinkan";
+            return json_encode($response);
+        }
+
+        $rules = [
+            'id' => [
+                'rules' => 'required|trim',
+                'errors' => [
+                    'required' => 'Id tidak boleh kosong. ',
+                ]
+            ],
+            'id_ptk' => [
+                'rules' => 'required|trim',
+                'errors' => [
+                    'required' => 'Id PTK tidak boleh kosong. ',
+                ]
+            ],
+            'tw' => [
+                'rules' => 'required|trim',
+                'errors' => [
+                    'required' => 'TW tidak boleh kosong. ',
+                ]
+            ],
+            'nama' => [
+                'rules' => 'required|trim',
+                'errors' => [
+                    'required' => 'Nama tidak boleh kosong. ',
+                ]
+            ],
+        ];
+
+        if (!$this->validate($rules)) {
+            $response = new \stdClass;
+            $response->status = 400;
+            $response->message = $this->validator->getError('id')
+                . $this->validator->getError('id_ptk')
+                . $this->validator->getError('tw')
+                . $this->validator->getError('nama');
+            return json_encode($response);
+        } else {
+            $id = htmlspecialchars($this->request->getVar('id'), true);
+            $id_ptk = htmlspecialchars($this->request->getVar('id_ptk'), true);
+            $tw = htmlspecialchars($this->request->getVar('tw'), true);
+            $nama = htmlspecialchars($this->request->getVar('nama'), true);
+
+            $current = $this->_db->table('v_lolosberkas_usulan_tpg a')
+                ->select("a.*, b.kecamatan as kecamatan_sekolah")
+                ->join('ref_sekolah b', 'a.npsn = b.npsn')
+                ->where(['a.id_usulan' => $id, 'a.id_tahun_tw' => $tw])->get()->getRowObject();
+
+            if ($current) {
+                $sptjm = $this->_db->table('_tb_sptjm_verifikasi')->where(['kode_usulan' => $current->kode_usulan, 'id_ptks' => $current->id_ptk_usulan])->get()->getRowObject();
+                if (!$sptjm) {
+                    $response = new \stdClass;
+                    $response->status = 400;
+                    $response->message = "Data SPTJM tidak ditemukan";
+                    return json_encode($response);
+                }
+
+                if ($sptjm->generate_sptjm == 1) {
+                    $response = new \stdClass;
+                    $response->status = 400;
+                    $response->message = "Data SPTJM telah digenerate.";
+                    return json_encode($response);
+                }
+
+                $this->_db->transBegin();
+                try {
+                    $this->_db->table('_tb_sptjm_verifikasi')->where('id', $sptjm->id)->delete();
+                    if ($this->_db->affectedRows() > 0) {
+                        $this->_db->table('_tb_usulan_detail_tpg')->where('id', $current->id_usulan)->update(['status_usulan' => 0, 'admin_approve' => NULL, 'date_approve' => NULL, 'keterangan_reject' => NULL]);
+                        if ($this->_db->affectedRows() > 0) {
+                            $this->_db->table('_upload_data_attribut')->where(['id_ptk' => $current->id_ptk, 'id_tahun_tw' => $current->id_tahun_tw])->update(['is_locked' => 1]);
+                            $this->_db->table('_absen_kehadiran')->where(['id_ptk' => $current->id_ptk, 'id_tahun_tw' => $current->id_tahun_tw])->update(['is_locked' => 1]);
+                            $this->_db->table('_ptk_tb')->where(['id_ptk' => $current->id_ptk])->update(['is_locked' => 1]);
+
+                            $this->_db->transCommit();
+                            $response = new \stdClass;
+                            $response->status = 200;
+                            $response->message = "Usulan $nama berhasil dikembalikan.";
+                            return json_encode($response);
+                        } else {
+                            $this->_db->transRollback();
+                            $response = new \stdClass;
+                            $response->status = 400;
+                            $response->message = "Gagal mengembalikan usulan $nama.";
+                            return json_encode($response);
+                        }
+                    } else {
+                        $this->_db->transRollback();
+                        $response = new \stdClass;
+                        $response->status = 400;
+                        $response->message = "Gagal mengembalikan usulan $nama.";
+                        return json_encode($response);
+                    }
+                } catch (\Throwable $th) {
+                    $this->_db->transRollback();
+                    $response = new \stdClass;
+                    $response->status = 400;
+                    $response->error = var_dump($th);
+                    $response->message = "Gagal mengembalikan usulan $nama.";
+                    return json_encode($response);
+                }
+            } else {
+                $response = new \stdClass;
+                $response->status = 400;
+                $response->message = "Data tidak ditemukan";
+                return json_encode($response);
+            }
+        }
+    }
+}
