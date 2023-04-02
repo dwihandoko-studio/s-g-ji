fbfa1db3e85b4ce74be2ee4b5358ea173f1ac3cd
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-04-02 22:54:03 +0700

    show page usulan su

--- Controllers/Situgu/Su/Us/Tpg/Lolosberkas.php
+++ Controllers/Situgu/Su/Us/Tpg/Lolosberkas.php
@@ -91,7 +91,7 @@ class Lolosberkas extends BaseController
             $row[] = $list->nik;
             $row[] = $list->nuptk;
             $row[] = $list->jenis_ptk;
-            $row[] = $list->created_at;
+            $row[] = $list->date_approve;
 
             $data[] = $row;
         }
