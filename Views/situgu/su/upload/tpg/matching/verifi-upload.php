a96e0e08ce00d46ec4ff3548d7040d988c58f1fc
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-04-05 04:33:26 +0700

    add page upload matching su

--- Views/situgu/su/upload/tpg/matching/verifi-upload.php
+++ Views/situgu/su/upload/tpg/matching/verifi-upload.php
@@ -37,7 +37,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-12">
+                    <div class="col-lg-12 d-none d-lg-block mb-2 mt-2">
                         <div>
                             <progress id="progressBar" value="0" max="100" style="width:100%; display: none;"></progress>
                         </div>
