2cf9b4b293c9679cba421af2927f6326f49a16bc
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-04-05 01:15:43 +0700

    add page upload matching su

--- Views/situgu/su/upload/tpg/matching/verifi-upload.php
+++ Views/situgu/su/upload/tpg/matching/verifi-upload.php
@@ -7,7 +7,7 @@
                         <div class="col-4">
                             <div>
                                 <p class="text-muted text-truncate mb-2">Jumlah Data Matching</p>
-                                <h5 class="mb-0 text-info result_total_data" id="result_total_data">0</h5>
+                                <h5 class="mb-0 text-info result_total" id="result_total">0</h5>
                             </div>
                         </div>
                         <div class="col-4">
@@ -234,12 +234,18 @@
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                const result_total = document.getElementById("result_total");
+                const result_total = document.getElementById(".result_total");
                 result_total.textContent = data.total.toString();
-                const result_lolos = document.getElementById("result_lolos");
+                const result_lolos = document.getElementById(".result_lolos");
                 result_lolos.textContent = data.lolos.toString();
-                const result_gagal = document.getElementById("result_gagal");
+                const result_gagal = document.getElementById(".result_gagal");
                 result_gagal.textContent = data.gagal.toString();
+                // const result_total = document.querySelector(".result_total");
+                // result_total.textContent = data.total.toString();
+                // const result_lolos = document.querySelector(".result_lolos");
+                // result_lolos.textContent = data.lolos.toString();
+                // const result_gagal = document.querySelector(".result_gagal");
+                // result_gagal.textContent = data.gagal.toString();
 
                 for (let i = 0; i < data.data.length; i++) {
                     const row = document.createElement("tr");
