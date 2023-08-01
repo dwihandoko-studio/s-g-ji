6feca7a962c2e00d4ec73573e499335f321fdacb
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-08-01 22:32:49 +0700

    add status permohonan peng

---
+++ Controllers/Silastri/Peng/Permohonan/Proses.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Controllers\Silastri\Peng\Permohonan;
+
+use App\Controllers\BaseController;
+use App\Models\Silastri\Peng\Permohonan\ProsesModel;
+use Config\Services;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use App\Libraries\Profilelib;
+use App\Libraries\Apilib;
+use App\Libraries\Helplib;
+use App\Libraries\Silastri\Ttelib;
+use App\Libraries\Uuid;
+
+class Proses extends BaseController
+{
+    var $folderImage = 'masterdata';
+    private $_db;
+    private $model;
+    private $_helpLib;
+
+    function __construct()
+    {
+        helper(['text', 'file', 'form', 'session', 'array', 'imageurl', 'web', 'filesystem']);
+        $this->_db      = \Config\Database::connect();
+        $this->_helpLib = new Helplib();
+    }
+
+    public function getAll()
+    {
+        $request = Services::request();
+        $datamodel = new ProsesModel($request);
+
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->status != 200) {
+            session()->destroy();
+            delete_cookie('jwt');
+            return redirect()->to(base_url('auth'));
+        }
+
+        $lists = $datamodel->get_datatables($user->data->id);
+        $data = [];
+        $no = $request->getPost("start");
+        foreach ($lists as $list) {
+            $no++;
+            $row = [];
+
+            $row[] = $no;
+            $action = '<a href="./detail?token=' . $list->id_permohonan . '"><button type="button" class="btn btn-primary btn-sm btn-rounded waves-effect waves-light mr-2 mb-1">
+                <i class="bx bxs-show font-size-16 align-middle"></i> DETAIL</button>
+                </a>';
+            $row[] = $action;
+            $row[] = $list->layanan;
+            $row[] = $list->kode_permohonan;
+            $row[] = $list->nik;
+            $row[] = str_replace('&#039;', "`", str_replace("'", "`", $list->nama));
+            $row[] = $list->kk;
+            $row[] = $list->jenis;
+
+            $data[] = $row;
+        }
+        $output = [
+            "draw" => $request->getPost('draw'),
+            "recordsTotal" => $datamodel->count_all($user->data->id),
+            "recordsFiltered" => $datamodel->count_filtered($user->data->id),
+            "data" => $data
+        ];
+        echo json_encode($output);
+    }
+
+    public function index()
+    {
+        return redirect()->to(base_url('silastri/peng/permohonan/proses/data'));
+    }
+
+    public function data()
+    {
+        $data['title'] = 'Permohonan Layanan Dalam Proses';
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->status != 200) {
+            session()->destroy();
+            delete_cookie('jwt');
+            return redirect()->to(base_url('auth'));
+        }
+
+        $data['user'] = $user->data;
+
+        // $data['jeniss'] = ['Surat Keterangan DTKS untuk Pengajuan PIP', 'Surat Keterangan DTKS untuk Pendaftaran PPDB', 'Surat Keterangan DTKS untuk Pengajuan PLN', 'Lainnya'];
+
+        return view('silastri/peng/permohonan/proses/index', $data);
+    }
+
+    public function detail()
+    {
+        if ($this->request->getMethod() != 'get') {
+            return view('404', ['error' => "Akses tidak diizinkan."]);
+        }
+
+        $data['title'] = 'Detail Proses Permohonan Layanan';
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->status != 200) {
+            session()->destroy();
+            delete_cookie('jwt');
+            return redirect()->to(base_url('auth'));
+        }
+
+        $data['user'] = $user->data;
+
+        $id = htmlspecialchars($this->request->getGet('token') ?? "", true);
+
+        $current = $this->_db->table('_permohonan a')
+            ->select("a.*, 
+                b.nik as nik_pemohon, 
+                b.kk as kk, 
+                b.email as email, 
+                b.no_hp as no_hp, 
+                b.tempat_lahir, 
+                b.tgl_lahir, 
+                b.jenis_kelamin, 
+                b.alamat, 
+                c.id as id_kecamatan, 
+                c.kecamatan as nama_kecamatan, 
+                d.id as id_kelurahan, 
+                d.kelurahan as nama_kelurahan")
+            ->join('_profil_users_tb b', 'b.id = a.user_id')
+            ->join('ref_kecamatan c', 'c.id = b.kecamatan')
+            ->join('ref_kelurahan d', 'd.id = b.kelurahan')
+            ->where(['a.id' => $id, 'a.status_permohonan' => 1])->get()->getRowObject();
+
+        if ($current) {
+            $data['data'] = $current;
+            return view('silastri/peng/permohonan/proses/detail-page', $data);
+        } else {
+            return view('404', ['error' => "Data tidak ditemukan."]);
+        }
+    }
+}
