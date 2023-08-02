19ad4b4f3aef934c6169816012b40f127be31c68
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-08-02 10:17:10 +0700

    add status permohonan peng

--- Views/silastri/peng/pengaduan/index.php
+++ Views/silastri/peng/pengaduan/index.php
@@ -24,7 +24,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <div class="card-header">
+                    <!-- <div class="card-header">
                         <div class="row">
                             <div class="col-6">
                                 <h4 class="card-title">Data Permohonan Layanan Pengaduan</h4>
@@ -41,7 +41,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="card-body">
                         <table id="data-datatables" class="table table-bordered dt-responsive  nowrap w-100">
                             <thead>
