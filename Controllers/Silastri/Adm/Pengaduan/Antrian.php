f98b29aa833492f10c5d1662d402a757ea7478a5
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-08-05 16:26:33 +0700

    add status permohonan peng

--- Controllers/Silastri/Adm/Pengaduan/Antrian.php
+++ Controllers/Silastri/Adm/Pengaduan/Antrian.php
@@ -232,7 +232,7 @@ class Antrian extends BaseController
             $data['id'] = $id;
             $data['nama'] = $nama;
             $data['data'] = $oldData;
-            $data['dinass'] = $oldData;
+            $data['dinass'] = [];
             $data['kecamatans'] = $this->_db->table('ref_kecamatan')->orderBy('kecamatan', 'ASC')->get()->getResult();
             $data['kelurahans'] = $this->_db->table('ref_kelurahan')->orderBy('kelurahan', 'ASC')->get()->getResult();
             $response = new \stdClass;
